79c42fb68ab4c792d9e1e172b976b780053b5366
cshyang <cshyang.chng@gmail.com>
2025-02-05 11:06:47 +0800

    Rename agents module

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,7 +1,7 @@
-from .topic_explorer import gen_related_topics
-from .outline_gen import gen_outline
-from .section_drafter import gen_section_drafts
-from .article_gen import gen_article
+from .agent_topic_explorer import gen_related_topics
+from .agent_outline_gen import gen_outline
+from .agent_section_drafter import gen_section_drafts
+from .agent_article_gen import gen_article
 
 __all__ = [
     "gen_related_topics",
